6ea584b7c73780cb79254eece13a8dc65b0f5851
Daniel <a21dangonsem@inspedralbes.cat>
2024-04-30 11:52:16 +0200

    Arreglando perfil

--- app/src/main/java/com/example/projectofinal/EditarPerfil.java
+++ app/src/main/java/com/example/projectofinal/EditarPerfil.java
@@ -1,4 +1,4 @@
-package com.example.projectetf2;
+package com.example.projectofinal;
 
 import android.content.res.Configuration;
 import android.os.Bundle;
@@ -29,40 +29,40 @@ public class EditarPerfil extends AppCompatActivity {
         TextView textViewCambiosGuardados = findViewById(R.id.textViewCambiosGuardados);
         ConstraintLayout firstLayout = findViewById(R.id.firstLayout);
         ConstraintLayout secondLayout = findViewById(R.id.secondLayout);
-        ConstraintLayout fourLayout = findViewById(R.id.fourLayout);
+        //ConstraintLayout fourLayout = findViewById(R.id.fourLayout);
         EditText editTextNC = findViewById(R.id.editTextNC);
         EditText editTextEmail = findViewById(R.id.editTextEmail);
-        EditText editTextPhone = findViewById(R.id.editTextPhone);
+       //EditText editTextPhone = findViewById(R.id.editTextPhone);
         ImageButton imageButtonClearNC = findViewById(R.id.imageButtonClear1);
         ImageButton imageButtonClearEmail = findViewById(R.id.imageButtonClear2);
-        ImageButton imageButtonClearPhone = findViewById(R.id.imageButtonClear3);
-        TextView textViewfecha = findViewById(R.id.textViewFecha);
+        /*ImageButton imageButtonClearPhone = findViewById(R.id.imageButtonClear3);
+        TextView textViewfecha = findViewById(R.id.textViewFecha);*/
         // Habilitar el TextView y cambiar su color a naranja cuando se detecten cambios
         textViewCambiosGuardados.setEnabled(true);
         textViewCambiosGuardados.setTextColor(getResources().getColor(R.color.naranja1));
         // Deshabilitar el TextView y cambiar su color a gris cuando los cambios se guarden
         textViewCambiosGuardados.setEnabled(false);
-        textViewCambiosGuardados.setTextColor(getResources().getColor(R.color.gris));
+        textViewCambiosGuardados.setTextColor(getResources().getColor(R.color.negro1));
 
 
         setupEditTextFocusChange(editTextNC, firstLayout, imageButtonClearNC);
         setupEditTextFocusChange(editTextEmail, secondLayout, imageButtonClearEmail);
-        setupEditTextFocusChange(editTextPhone, fourLayout, imageButtonClearPhone);
+        //setupEditTextFocusChange(editTextPhone, fourLayout, imageButtonClearPhone);
 
-        setupImageButtonClear(editTextNC, imageButtonClearNC);
-        setupImageButtonClear(editTextEmail, imageButtonClearEmail);
-        setupImageButtonClear(editTextPhone, imageButtonClearPhone);
+        //setupImageButtonClear(editTextNC, imageButtonClearNC);
+        //setupImageButtonClear(editTextEmail, imageButtonClearEmail);
+        //setupImageButtonClear(editTextPhone, imageButtonClearPhone);
 
 
-        textViewfecha.setOnClickListener(new View.OnClickListener() {
+        /*textViewfecha.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 showDatePicker();
             }
-        });
+        });*/
     }
 
-    private void setupEditTextFocusChange(EditText editText, ConstraintLayout layout, ImageButton button) {
+    private void setupEditTextFocusChange(EditText editText, ConstraintLayout layout,ImageButton button) {
         editText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
             public void onFocusChange(View v, boolean hasFocus) {
@@ -93,7 +93,7 @@ public class EditarPerfil extends AppCompatActivity {
         datePickerFragment.show(fragmentManager, "DatePicker");
     }
 
-    public void processDatePickerResult(int year, int month, int day) {
+    /*public void processDatePickerResult(int year, int month, int day) {
         String dateMessage="";
         String month_string = Integer.toString(month + 1);
         String day_string = Integer.toString(day);
@@ -102,6 +102,6 @@ public class EditarPerfil extends AppCompatActivity {
                 + month_string + "/" + year_string);
         TextView selectedDateTextView = findViewById(R.id.textViewFecha);
         selectedDateTextView.setText(getString(R.string.selected_date, dateMessage));
-    }
+    }*/
 
 }
