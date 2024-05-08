0131266ed69c6b5037468d9e207a20aea303446f
Daniel <a21dangonsem@inspedralbes.cat>
2024-05-08 09:40:46 +0200

    AÑadir amigo

--- app/src/main/java/com/example/projectofinal/FriendRequestBody.java
+++ app/src/main/java/com/example/projectofinal/FriendRequestBody.java
@@ -3,6 +3,13 @@ package com.example.projectofinal;
 public class FriendRequestBody {
     private int senderId;
     private int receiverId;
+    private String status;
+
+    public FriendRequestBody(int senderId, int receiverId, String status) {
+        this.senderId = senderId;
+        this.receiverId = receiverId;
+        this.status = status;
+    }
 
     public FriendRequestBody(int senderId, int receiverId) {
         this.senderId = senderId;
@@ -23,5 +30,13 @@ public class FriendRequestBody {
 
     public void setReceiverId(int receiverId) {
         this.receiverId = receiverId;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
     }
 }
