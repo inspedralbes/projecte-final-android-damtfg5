f69e6a38df61f7eb98b652dc9829558b31550d3c
Daniel <a21dangonsem@inspedralbes.cat>
2024-05-08 10:56:27 +0200

    AÑadir amigo

--- app/src/main/java/com/example/projectofinal/Notifications.java
+++ app/src/main/java/com/example/projectofinal/Notifications.java
@@ -5,14 +5,23 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class Notifications extends AppCompatActivity {
     private RecyclerView recyclerView;
     private NotificacionesAdapter adapter;
     private List<Usuario> notificaciones;
+    private String URL = "http://192.168.206.176:3001/";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,5 +34,41 @@ public class Notifications extends AppCompatActivity {
         adapter = new NotificacionesAdapter(this, notificaciones);
 
         recyclerView.setAdapter(adapter);
+
+        obtenerSolicitudesAmistadPendientes(5);
+    }
+
+    private void obtenerSolicitudesAmistadPendientes(int userId) {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        ApiService apiService = retrofit.create(ApiService.class);
+        Call<List<FriendRequestBody>> call = apiService.getPendingFriendRequests(userId);
+
+        call.enqueue(new Callback<List<FriendRequestBody>>() {
+            @Override
+            public void onResponse(Call<List<FriendRequestBody>> call, Response<List<FriendRequestBody>> response) {
+                if (response.isSuccessful()) {
+                    List<FriendRequestBody> friendRequests = response.body();
+                    // Convertir las solicitudes de amistad en objetos Usuario
+                    for (FriendRequestBody friendRequest : friendRequests) {
+                        Usuario usuario = new Usuario(friendRequest.getSenderId(), "", "", "");
+                        notificaciones.add(usuario);
+                    }
+                    adapter.notifyDataSetChanged();
+                } else {
+                    // Manejar el caso en el que la respuesta del servidor no sea exitosa
+                    Toast.makeText(Notifications.this, "Error al obtener las solicitudes de amistad", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<FriendRequestBody>> call, Throwable t) {
+                // Manejar el caso en el que la llamada al servidor falle
+                Toast.makeText(Notifications.this, "Error de conexión", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
